9edbda8482956269f80bc5cd8f68e1079348b664
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-01 01:30:54 +0700

    Update youtube.js

--- utils/youtube.js
+++ utils/youtube.js
@@ -1,5 +1,5 @@
 const ytsr = require('ytsr')
-const ytdl = require("ytdl-core")
+//const ytdl = require("ytdl-core")
 const { default: axios } = require("axios")
 const { UserAgent } = require("./index")
 /*
