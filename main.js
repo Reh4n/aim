90b44da413932abcbc483c887db52d5b1d8cb5df
Ripp <75048902+Akkun3704@users.noreply.github.com>
2021-12-31 11:12:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const { color } = require('./utils');
 const { buttonsParser } = require('./core/parser');
 const cron = require('./utils/cronjob');
 const fs = require('fs');
+const util = require('util');
+const { exec } = require('child_process');
 const moment = require('moment-timezone');
 const djs = require('@discordjs/collection');
 const joinHandler = require("./group_event");
@@ -18,15 +20,15 @@ const cooldown = new djs.Collection();
 djs.prefix = prefix;
 
 function readCmd() {
-  let dir = fs.readdirSync('./commands');
-  dir.forEach(async (res) => {
-    const commandFiles = fs.readdirSync(`./commands/${res}`).filter((file) => file.endsWith('.js'));
-    for (const file of commandFiles) {
-      const command = require(`./commands/${res}/${file}`);
-      djs.commands.set(command.name, command);
-    }
-  });
-  console.log(color('[SYS]', 'yellow'), 'command loaded!');
+	let dir = fs.readdirSync('./commands');
+	dir.forEach(async (res) => {
+		const commandFiles = fs.readdirSync(`./commands/${res}`).filter((file) => file.endsWith('.js'));
+		for (const file of commandFiles) {
+			const command = require(`./commands/${res}/${file}`);
+			djs.commands.set(command.name, command);
+		}
+	});
+	console.log(color('[SYS]', 'yellow'), 'command loaded!');
 }
 readCmd();
 
@@ -34,119 +36,109 @@ con.connect();
 process.on('uncaughtException', console.error);
 
 function printLog(isCmd, sender, groupName, isGroup) {
-  const time = moment.tz('Asia/Jakarta').format('DD/MM/YY HH:mm:ss');
-  if (isCmd && isGroup) {
-    return console.log(
-      color(`[${time}]`, 'yellow'),
-      color('[EXEC]', 'aqua'),
-      color(`${sender.split('@')[0]}`, 'lime'),
-      'in',
-      color(`${groupName}`, 'lime'),
-    );
-  }
-  if (isCmd && !isGroup) {
-    return console.log(color(`[${time}]`, 'yellow'), color('[EXEC]', 'aqua'), color(`${sender.split('@')[0]}`, 'lime'));
-  }
+	const time = moment.tz('Asia/Jakarta').format('DD/MM/YY HH:mm:ss');
+	if (isCmd && isGroup) {
+		return console.log(color(`[${time}]`, 'yellow'), color('[EXEC]', 'aqua'), color(`${sender.split('@')[0]}`, 'lime'), 'in', color(`${groupName}`, 'lime'));
+	}
+	if (isCmd && !isGroup) {
+		return console.log(color(`[${time}]`, 'yellow'), color('[EXEC]', 'aqua'), color(`${sender.split('@')[0]}`, 'lime'));
+	}
 }
 
 /* Cron */
 cron(ev);
 
 ev.on('CB:action,,battery', (b) => {
-  ev['battery']['value'] = parseInt(b[2][0][1].value)
-  ev['battery']['charge'] = b[2][0][1].live === 'false' ? false : true
-  ev['battery']['lowPower'] = b[2][0][1].powersave === 'false' ? false : true
+	ev['battery']['value'] = parseInt(b[2][0][1].value)
+	ev['battery']['charge'] = b[2][0][1].live === 'false' ? false : true
+	ev['battery']['lowPower'] = b[2][0][1].powersave === 'false' ? false : true
 });
 
 ev.on("group-participants-update", async (json) => {
-  joinHandler(json)
+	joinHandler(json)
 });
 
 ev.on('chat-update', async (msg) => {
-  try {
-    if (!msg.hasNewMessage) return;
-    msg = await wa.serialize(msg);
-    if (!msg.message) return;
-    if (msg.key && msg.key.remoteJid === 'status@broadcast') return;
-    // if (!msg.key.fromMe) return;
-    let { body } = msg;
-    let temp_pref = multi_pref.test(body) ? body.split('').shift() : '!';
-    if (body === 'prefix' || body === 'cekprefix') {
-      wa.reply(msg.from, `My prefix ${prefix}`, msg);
-    }
-    const { type, isGroup, sender, from } = msg;
-    if (type === 'buttonsResponseMessage' && !body.startsWith(temp_pref)) {
-      buttonsParser(msg);
-    } else if (type === 'buttonsResponseMessage' && body.startsWith(temp_pref)) {
-      msg.message = { conversation: msg.message.buttonsResponseMessage.selectedDisplayText };
-    }
-    body =
-      type === 'conversation' && body.startsWith(temp_pref)
-        ? body : (type === 'imageMessage' || type === 'videoMessage') && body && body.startsWith(temp_pref)
-          ? body : type === 'ephemeralMessage' && body.startsWith(temp_pref)
-            ? body : type === 'extendedTextMessage' && body.startsWith(temp_pref)
-              ? body : type === 'buttonsResponseMessage' && body.startsWith(temp_pref)
-                ? body : type === 'listResponseMessage' && body.startsWith(temp_pref) ? body : '';
-    const arg = body.substring(body.indexOf(' ') + 1);
-    const args = body.trim().split(/ +/).slice(1);
-    const isCmd = body.startsWith(temp_pref);
+	try {
+		if (!msg.hasNewMessage) return;
+		msg = await wa.serialize(msg);
+		if (!msg.message) return;
+		if (msg.key && msg.key.remoteJid === 'status@broadcast') return;
+		// if (!msg.key.fromMe) return;
+		let { type, isGroup, sender, from, body } = msg;
+		let temp_pref = multi_pref.test(body) ? body.split('').shift() : '!';
+		if (body === 'prefix' || body === 'cekprefix') {
+			wa.reply(from, `My prefix ${prefix}`, msg);
+		}
+		if (type === 'buttonsResponseMessage' && !body.startsWith(temp_pref)) {
+			buttonsParser(msg);
+		} else if (type === 'buttonsResponseMessage' && body.startsWith(temp_pref)) {
+			msg.message = { conversation: msg.message.buttonsResponseMessage.selectedDisplayText };
+		}
+		body = type === 'conversation' && body.startsWith(temp_pref) ? body : (type === 'imageMessage' || type === 'videoMessage') && body && body.startsWith(temp_pref) ? body : type === 'ephemeralMessage' && body.startsWith(temp_pref) ? body : type === 'extendedTextMessage' && body.startsWith(temp_pref) ? body : type === 'buttonsResponseMessage' && body.startsWith(temp_pref) ? body : type === 'listResponseMessage' && body.startsWith(temp_pref) ? body : '';
+		const arg = body.substring(body.indexOf(' ') + 1);
+		const args = body.trim().split(/ +/).slice(1);
+		const isCmd = body.startsWith(temp_pref);
 
-    const groupMeta = isGroup ? await ev.groupMetadata(from) : '';
-    const groupSubject = isGroup ? groupMeta.subject : '';
+		const groupMeta = isGroup ? await ev.groupMetadata(from) : '';
+		const groupSubject = isGroup ? groupMeta.subject : '';
 
-    // Log
-    printLog(isCmd, sender, groupSubject, isGroup);
+		// Log
+		printLog(isCmd, sender, groupSubject, isGroup);
+		
+		if (/^>?> /.test(body)) {
+			let teks
+			try { teks = await eval(`(async () => { ${(/^>>/.test(body) ? 'return ' : '') + args.join(' ')} })()`) }
+			catch (e) { teks = e }
+			finally { wa.reply(from, util.format(teks), msg) }
+		}
+		if (/^[$] /.test(body)) {
+			await wa.reply(from, 'Executing...', msg)
+			exec(args.join(' '), (error, stdout) => {
+				if (error) wa.reply(from, String(error), msg)
+				else wa.reply(from, String(stdout), msg)
+			})
+		}
 
-    const commandName = body.slice(prefix.length).trim().split(/ +/).shift().toLowerCase();
-    const command =
-      djs.commands.get(commandName) || djs.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+		const commandName = body.slice(prefix.length).trim().split(/ +/).shift().toLowerCase();
+		const command = djs.commands.get(commandName) || djs.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
 
-    if (!command) return;
+		if (!command) return;
 
-    if (!cooldown.has(from)) {
-      cooldown.set(from, new djs.Collection());
-    }
+		if (!cooldown.has(from)) {
+			cooldown.set(from, new djs.Collection());
+		}
 
-    const now = Date.now();
-    const timestamps = cooldown.get(from);
-    const cooldownAmount = (command.cooldown || 5) * 1000;
+		const now = Date.now();
+		const timestamps = cooldown.get(from);
+		const cooldownAmount = (command.cooldown || 5) * 1000;
 
-    if (timestamps.has(from)) {
-      const expirationTime = timestamps.get(from) + cooldownAmount;
+		if (timestamps.has(from)) {
+			const expirationTime = timestamps.get(from) + cooldownAmount;
 
-      if (now < expirationTime) {
-        if (isGroup) {
-          let timeLeft = (expirationTime - now) / 1000;
-          console.log(
-            color(`[${time}]`, 'yellow'),
-            color('[SPAM]', 'red'),
-            color(`${sender.split('@')[0]}`, 'lime'),
-            'in',
-            color(groupSubject, 'lime'),
-          );
-          return wa.reply(from, `This group is on cooldown, please wait another _${timeLeft.toFixed(1)} second(s)_`, msg)
-        }
-        if (!isGroup) {
-          let timeLeft = (expirationTime - now) / 1000;
-          console.log(
-            color(`[${time}]`, 'yellow'),
-            color('[SPAM]', 'red'),
-            color(`${sender.split('@')[0]}`, 'lime'),
-          );
-          return wa.reply(from, `You are on cooldown, please wait another _${timeLeft.toFixed(1)} second(s)_`, msg)
-        }
-      }
-    }
+			if (now < expirationTime) {
+				if (isGroup) {
+					let timeLeft = (expirationTime - now) / 1000;
+					console.log(color(`[${time}]`, 'yellow'), color('[SPAM]', 'red'), color(`${sender.split('@')[0]}`, 'lime'), 'in', color(groupSubject, 'lime'));
+					return wa.reply(from, `This group is on cooldown, please wait another _${timeLeft.toFixed(1)} second(s)_`, msg)
+				}
+				if (!isGroup) {
+					let timeLeft = (expirationTime - now) / 1000;
+					console.log(color(`[${time}]`, 'yellow'), color('[SPAM]', 'red'), color(`${sender.split('@')[0]}`, 'lime'));
+					return wa.reply(from, `You are on cooldown, please wait another _${timeLeft.toFixed(1)} second(s)_`, msg)
+				}
+			}
+		}
 
-    timestamps.set(from, now);
-    setTimeout(() => timestamps.delete(from), cooldownAmount);
+		timestamps.set(from, now);
+		setTimeout(() => timestamps.delete(from), cooldownAmount);
 
-    try {
-      command.execute(msg, wa, args, arg);
-    } catch (err) {
-      console.error(err);
-    }
-  } catch (e) {
-    console.log(`Error: ${e}`);
-  }
+		try {
+			command.execute(msg, wa, args, arg);
+		} catch (err) {
+			console.error(err);
+		}
+	} catch (e) {
+		console.log(`Error: ${e}`);
+	}
 });
