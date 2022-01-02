3d4fb30990c9db6785e363e66c5787ed905a6d17
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-02 23:23:19 +0700

    Update mynimeku_download.js

--- commands/weeb/mynimeku_download.js
+++ commands/weeb/mynimeku_download.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const { toPDF } = require('../../utils')
+const ev = require('../../core/connect').Whatsapp
 
 module.exports = {
 	name: 'mynimekudl',
@@ -9,24 +10,28 @@ module.exports = {
 	use: '[options] url\n\n- *Options* -\n\n1. anime\n2. manga\n\nEx: !mynimeku anime url',
 	async execute(msg, wa, args) {
 		try {
+			const { from } = msg
 			let data
 			switch (args[0].toLowerCase()) {
 				case 'anime':
+					if (!args[1]) return wa.reply(from, 'URL needed', msg)
 					data = await downloadNimek(args[1])
 					console.log(data)
-					await ev.sendMessage(msg.from, { url: data }, 'videoMessage', { quoted: msg })
+					await ev.sendMessage(from, { url: data }, 'videoMessage', { quoted: msg })
 				break
 				case 'manga': case 'komik':
+					if (!args[1]) return wa.reply(from, 'URL needed', msg)
 					let { title, result } = await downloadKomik(args[1])
 					let { data: thumbnail } = await axios.get(result[0], { responseType: 'arraybuffer' })
 					console.log(title, result)
 					data = await toPDF(result)
-					await ev.sendMessage(msg.from, data, 'documentMessage', { quoted: msg, filename: `${title}.pdf`, thumbnail })
+					await ev.sendMessage(from, data, 'documentMessage', { quoted: msg, filename: `${title}.pdf`, thumbnail })
 				break
-				default: 
+				default:
+					if (!args[0]) return wa.reply(from, 'URL needed', msg)
 					data = await downloadNimek(args[0])
 					console.log(data)
-					await ev.sendMessage(msg.from, { url: data }, 'videoMessage', { quoted: msg })
+					await ev.sendMessage(from, { url: data }, 'videoMessage', { quoted: msg })
 			}
 		} catch (e) {
 			console.log(e)
