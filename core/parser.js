296124b54dede01baf3bb985ff20ee0f2c3214f6
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-03 03:52:32 +0700

    Update parser.js

--- core/parser.js
+++ core/parser.js
@@ -14,7 +14,7 @@ module.exports = {
         })
         switch (context) {
             case "telegram bot":
-                await ev.sendMessage(from, "t.me/SecondMidnight_bot\nMy Telegram Bot\nThanks for using my bot.", "conversation", { detectLinks: false, quoted: e })
+                await ev.sendMessage(from, "t.me/Nyarlathotepp_bot\nMy Telegram Bot\nThanks for using my bot.", "conversation", { detectLinks: false, quoted: e })
                 break;
             case "":
                 break;
