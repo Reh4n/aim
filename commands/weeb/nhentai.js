7509ac1eebfaba1ed16ce62510471490e6ec8bb9
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-04 18:16:44 +0700

    Update nhentai.js

--- commands/weeb/nhentai.js
+++ commands/weeb/nhentai.js
@@ -1,12 +1,12 @@
-const ev = require('../../core/connect').Whatsapp
 const { fetchBuffer, toPDF } = require('../../utils')
+const { generateMessageID } = require('@adiwajshing/baileys')
 const { getLatest, search, getDoujin, getPopular, random } = require('nhentai-node-api')
 
 module.exports = {
 	name: 'nhentai',
 	category: 'weebs',
 	desc: 'Download/Search doujin hentai from web nhentai.',
-	use: '[options] quey\n\n- *Options* -\n\n1. pdf\n2. search\n3. latest\n\nEx: !nhentai pdf 212121',
+	use: '[options] query|code\n\n- *Options* -\n\n1. pdf\n2. search\n3. latest\n\nEx: !nhentai pdf 212121',
 	async execute(msg, wa, args) {
 		try {
 			const { from } = msg
@@ -15,7 +15,7 @@ module.exports = {
 				case 'latest': {
 					let res = await getLatest()
 					let thumbnail = await fetchBuffer(res[0].thumbnail)
-					await wa.custom(from, res.map((v, i) => `${i + 1}. ${v.title}`).join('\n\n'), 'extendedTextMessage', { quoted: msg, contextInfo: { externalAdReply: { title: res[0].title, body: 'Nhentai Latest', thumbnail, sourceUrl: 'https://hiken.xyz/v/' + res[0].id }}})
+					await wa.custom(from, res.map((v, i) => `${i + 1}. ${v.title}`).join('\n\n'), 'extendedTextMessage', { quoted: msg, messageId: generateMessageID().slice(0, 5) + 'NHENTAI', contextInfo: { externalAdReply: { title: res[0].title, body: 'Nhentai Latest', thumbnail, sourceUrl: 'https://hiken.xyz/v/' + res[0].id }}})
 					break
 				}
 				case 'search': {
@@ -24,7 +24,7 @@ module.exports = {
 					let res = await search(args.splice(1).join(' '))
 					console.log(args.splice(1).join(' '))
 					let thumbnail = await fetchBuffer(res[0].thumbnail)
-					await wa.custom(from, res.map((v, i) => `${i + 1}. ${v.title}`).join('\n\n'), 'extendedTextMessage', { quoted: msg, contextInfo: { externalAdReply: { title: res[0].title, body: 'Nhentai Search', thumbnail, sourceUrl: 'https://hiken.xyz/v/' + res[0].id }}})
+					await wa.custom(from, res.map((v, i) => `${i + 1}. ${v.title}`).join('\n\n'), 'extendedTextMessage', { quoted: msg, messageId: generateMessageID().slice(0, 5) + 'NHENTAI', contextInfo: { externalAdReply: { title: res[0].title, body: 'Nhentai Search', thumbnail, sourceUrl: 'https://hiken.xyz/v/' + res[0].id }}})
 					break
 				}
 				case 'pdf': {
@@ -45,10 +45,18 @@ module.exports = {
 					break
 				}
 				default:
+				if (args[0] && typeof args[0] == 'number') {
+					await wa.reply(from, 'Loading...', msg)
+					let { title, cover, pages } = await getDoujin(args[0])
+					pages = await toPDF(pages)
+					let thumbnail = await fetchBuffer(cover)
+					await wa.custom(from, pages, 'documentMessage', { quoted: msg, filename: `${title.default}.pdf`, mimetype: 'application/pdf', thumbnail })
+				} else {
 					await wa.reply(from, 'Loading...', msg)
 					let res = await getPopular()
 					let thumbnail = await fetchBuffer(res[0].thumbnail)
-					await wa.custom(from, res.map((v, i) => `${i + 1}. ${v.title}`).join('\n\n'), 'extendedTextMessage', { quoted: msg, contextInfo: { externalAdReply: { title: res[0].title, body: 'Nhentai Popular', thumbnail, sourceUrl: 'https://hiken.xyz/v/' + res[0].id }}})
+					await wa.custom(from, res.map((v, i) => `${i + 1}. ${v.title}`).join('\n\n'), 'extendedTextMessage', { quoted: msg, messageId: generateMessageID().slice(0, 5) + 'NHENTAI', contextInfo: { externalAdReply: { title: res[0].title, body: 'Nhentai Popular', thumbnail, sourceUrl: 'https://hiken.xyz/v/' + res[0].id }}})
+				}
 			}
 		} catch (e) {
 			console.log(e)
