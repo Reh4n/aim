6b61a953e60465e0522b9ada75e7f32b0de2a6c6
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-07 09:23:30 +0700

    Create runtime.js

---
+++ commands/general/runtime.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: 'runtime',
+	aliases: ['uptime'],
+	category: 'info',
+	async execute(msg, wa) {
+		wa.reply(msg.from, clockString(processs.uptime()), msg)
+	}
+}
+
+function clockString(ms) {
+	let d = isNaN(ms) ? '--' : Math.floor(ms / (3600 * 24))
+	let h = isNaN(ms) ? '--' : Math.floor(ms % (3600 * 24) / 3600)
+	let m = isNaN(ms) ? '--' : Math.floor(ms % 3600 / 60)
+	let s = isNaN(ms) ? '--' : Math.floor(ms % 60)
+	let dDisplay = d > 0 ? d + (d == 1 ? ' Hari ' : ' Hari ') : ''
+	let hDisplay = h > 0 ? h + (h == 1 ? ' Jam ' : ' Jam ') : ''
+	let mDisplay = m > 0 ? m + (m == 1 ? ' Menit ' : ' Menit ') : ''
+	let sDisplay = s > 0 ? s + (s == 1 ? ' Detik' : ' Detik') : ''
+	return dDisplay + hDisplay + mDisplay + sDisplay
+}
