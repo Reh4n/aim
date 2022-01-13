2e0ae90c5bc3130e68d634b6197035f632b42523
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-13 12:34:18 +0700

    Update join.js

--- commands/misc/join.js
+++ commands/misc/join.js
@@ -13,7 +13,7 @@ module.exports = {
         // check invite code
         const check = await ev.query({ json: ["query","invite",code] });
         if (check.status !== 200) return wa.reply(msg.from, "Invalid invite url.", msg);
-        if (check.size < 80) return wa.reply(msg.from, "The minimum requirement for group members must be more than 80 people.", msg);
+        //if (check.size < 80) return wa.reply(msg.from, "The minimum requirement for group members must be more than 80 people.", msg);
         // Trying to join group with given invite code
         const check2 = await ev.acceptInvite(code);
         if (check2.status !== 200) return wa.reply(msg.from, "Looks like the group already full or became invalid when I'm trying to join :/", msg);
