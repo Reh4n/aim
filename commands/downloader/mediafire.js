8850e99a09ebd79c5b359e3057914ad64c31f6f6
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-15 13:43:49 +0700

    Update and rename zippyshare.js to mediafire.js

---
+++ commands/downloader/mediafire.js
@@ -0,0 +1,38 @@
+const axios = require('axios')
+const { load } = require('cheerio')
+const { fromBuffer } = require('file-type')
+const { fetchBuffer } = require('../../utils')
+
+module.exports = {
+	name: 'mediafire',
+	category: 'downloader',
+	async execute(msg, wa, args) {
+		try {
+			const { from } = msg
+			if (!args[0]) return wa.reply(from, 'Input url', msg)
+			// await wa.reply(from, 'Loading...', msg)
+			let res = await mediafireDl(args[0])
+			await wa.reply(from, String(res), msg)
+			let buff = await fetchBuffer(res.link)
+			let { ext, mime: mimetype } = await fromBuffer(buff)
+			let filename = `${res.title}.${ext}`
+			await wa.custom(from, buff, 'documentMessage', { quoted: msg, filename, mimetype })
+		} catch (e) {
+			console.log(e)
+			wa.reply(msg.from, String(e), msg)
+		}
+	}
+}
+
+function mediafireDl(url) {
+	return new Promise((res, rej) => {
+		if (!/https?:\/\//.test(url)) return reject('Invalid url!')
+		axios(url).then(c => {
+			let $ = load(c.data)
+			let title = $('div.dl-btn-label').attr('title')
+			let size = $('a#downloadButton').text().split('\n')[1].replace(/ /g, '').replace(/\(|\)/g, '').replace('Download', '')
+			let link = $('a#downloadButton').attr('href')
+			res({ title, size, link })
+		}).catch(rej)
+	})
+}
