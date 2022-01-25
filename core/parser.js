22cc67d917caf19201c46ca75d54bba9ad2c02b0
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-26 04:44:37 +0700

    Update parser.js

--- core/parser.js
+++ core/parser.js
@@ -1,23 +1,18 @@
-const ev = require('./connect').Whatsapp
-const { WAMessageProto, MessageType } = require('@adiwajshing/baileys')
+const { Whatsapp: ev } = require('./connect')
 
 module.exports = {
-    async buttonsParser(m) {
-        const { from } = m
-        let context = m.message.buttonsResponseMessage.selectedDisplayText.toLowerCase()
-        let e = WAMessageProto.WebMessageInfo.fromObject({
-            key: { ...m.key },
-            message: {
-                conversation: context
-            },
-            timestamp: m.timestamp
-        })
-        switch (context) {
-            case "source code":
-                await ev.sendMessage(from, "https://github.com/FaizBastomi/wbot/", "conversation", { detectLinks: true, quoted: e })
-                break;
-            case "":
-                break;
-        }
-    }
+	async buttonsParser(m) {
+		const { from } = m
+		let context = m.message.buttonsResponseMessage.selectedDisplayText.toLowerCase()
+		switch (context) {
+			case "source code": {
+				await ev.sendMessage(from, "https://github.com/FaizBastomi/wbot/", "extendedTextMessage", { detectLinks: true, quoted: m })
+				break
+			}
+			case "owner": {
+				await ev.sendMessage(from, { displayName: 'Owner', vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Ripp\nTEL;type=WORK;waid=212706611366:212706611366\nEND:VCARD`.trim() }, 'contactMessage', { quoted: m })
+				break
+			}
+		}
+	}
 }
