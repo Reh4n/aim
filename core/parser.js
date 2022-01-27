df3662101c1549bd626d168e713dba7a7da8b6f4
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-01-28 04:57:23 +0700

    Update parser.js

--- core/parser.js
+++ core/parser.js
@@ -9,10 +9,6 @@ module.exports = {
 				await ev.sendMessage(from, "https://github.com/FaizBastomi/wbot/", "extendedTextMessage", { detectLinks: true, quoted: m })
 				break
 			}
-			case "owner": {
-				await ev.sendMessage(from, { displayName: 'Owner', vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Ripp\nTEL;type=WORK;waid=212706611366:212706611366\nEND:VCARD`.trim() }, 'contactMessage', { quoted: m })
-				break
-			}
 		}
 	}
 }
