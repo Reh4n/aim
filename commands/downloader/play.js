732b609ebdf8f1c81bc319db542430c26450face
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-01-28 09:28:03 +0700

    Update play.js

--- commands/downloader/play.js
+++ commands/downloader/play.js
@@ -23,13 +23,13 @@ module.exports = {
 				buttonText: { displayText: 'Video' }, buttonId: `#ytv ${s[0].url} SMH`, type: 1
 			}]
 		}
-		await wa.custom(from, struct, 'buttonsMessage', { quoted: msg }).then(async (msg) => {
+		await ev.sendMessage(from, struct, 'buttonsMessage', { quoted: msg }).then(async (msg) => {
 			try {
 				if (res.length > 15000000) {
 					let caption = `*Title:* ${items[0].title}\n*Views:* ${items[0].views}\n*Duration:* ${items[0].duration}\n*Download:* ${dl_link}\n\n_Filesize too big_`
-					wa.mediaURL(from, s[0].thumbnails[0].url, { quoted: msg, caption })
+					await wa.mediaURL(from, s[0].thumbnails[0].url, { quoted: msg, caption })
 				} else {
-					wa.custom(from, res, 'audioMessage', { mimetype: 'audio/mp4', quoted: msg })
+					await wa.custom(from, res, 'audioMessage', { mimetype: 'audio/mp4', quoted: msg })
 				}
 			} catch (e) {
 				wa.reply(msg.from, String(e), msg)
