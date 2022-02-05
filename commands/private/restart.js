8eb0a258060a3d20284876d316e05b9078007197
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-06 06:47:19 +0700

    Update restart.js

--- commands/private/restart.js
+++ commands/private/restart.js
@@ -9,9 +9,10 @@ module.exports = {
   async execute(msg, wa, args) {
     const { sender, from } = msg;
     if (!owner.includes(sender)) return;
-    pm2.restart('main', (err) => {
-      if (err) return wa.reply(from, `E: ${err}`, msg);
-    });
+    // pm2.restart('main', (err) => {
+    //  if (err) return wa.reply(from, `E: ${err}`, msg);
+    // });
+    process.send('reset')
     wa.reply(from, 'sukses', msg);
   },
 };
