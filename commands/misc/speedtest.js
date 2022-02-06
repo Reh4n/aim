58d3ae000480446f348885328f4548924df0e181
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-06 10:31:08 +0700

    Update speedtest.js

--- commands/misc/speedtest.js
+++ commands/misc/speedtest.js
@@ -2,9 +2,9 @@ module.exports = {
 	name: 'speedtest',
 	category: 'general',
 	execute(msg, wa) {
-		require('child_process').exec('speed-test -j', (error, stdout, stderr) => {
+		require('child_process').exec('speed-test -j', (error, stdout) => {
 			if (error) return wa.reply(msg.from, String(error), msg)
-			if (stderr) return wa.reply(msg.from, `Stderr: ${stderr}`, msg)
+			// if (stderr) return wa.reply(msg.from, `Stderr: ${stderr}`, msg)
 			wa.reply(msg.from, `Output: ${stdout}`, msg)
 		})
 	}
