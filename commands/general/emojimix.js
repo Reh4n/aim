dbaf6affc70143d27a655a61bf0171171314f6a0
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-07 09:35:52 +0700

    Update emojimix.js

--- commands/general/emojimix.js
+++ commands/general/emojimix.js
@@ -9,11 +9,11 @@ module.exports = {
 			let emo = args.join(' ').split('+')
 			if (!emo[0]) return wa.reply(msg.from, 'Emoji?', msg)
 			if (!emo[1]) return wa.reply(msg.from, 'Ex:\n!emojimix 😱+🥴', msg)
-			emo = emo.split('+').map(v => 'u' + v.codePointAt(0).toString(16))
+			emo = emo.map(v => 'u' + v.codePointAt(0).toString(16))
 			sticker((await fetchBuffer(`https://www.gstatic.com/android/keyboard/emojikitchen/20201001/${emo[0]}/${emo[0]}_${emo[1]}.png`)), { isImage: true, withPackInfo: true, cmdType: '2', packInfo: { packname: 'EmojiMix', author: '' }})
 			.then((v) => wa.sticker(msg.from, v, { quoted: msg }))
 		} catch (e) {
-                        console.log(e)
+			console.log(e)
 			wa.reply(msg.from, 'Error while processing your request...', msg)
 		}
 	}
