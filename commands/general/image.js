8db41c6b7bb11f8313ffffc70314749450a5005c
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-10 06:14:42 +0700

    Update image.js

--- commands/general/image.js
+++ commands/general/image.js
@@ -2,13 +2,13 @@ const { googleImage } = require('@bochilteam/scraper')
 
 module.exports = {
   name: 'image',
-  aliases: ['gimage', 'googleimage'],
+  aliases: ['img', 'gimage', 'googleimage'],
   category: 'general',
   async execute(msg, wa, args) {
     if (!args.join(' ')) wa.reply(msg.from, 'Input Query', msg)
     await wa.reply(msg.from, 'Loading...', msg)
     googleImage(args.join(' ')).then(res => {
-      wa.mediaURL(from, res, { quoted: msg, caption: `Hasil Pencarian: ${args.join(' ')}\nUrl: ${res}` })
+      wa.mediaURL(msg.from, res, { quoted: msg, caption: `Hasil Pencarian: ${args.join(' ')}\nUrl: ${res}` })
     }).catch(e => wa.reply(msg.from, String(e), msg))
   }
 }
