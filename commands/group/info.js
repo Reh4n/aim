7da411da87cc546c526797b95ed1c76b1d03741a
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-10 06:09:18 +0700

    Update info.js

--- commands/group/info.js
+++ commands/group/info.js
@@ -13,7 +13,7 @@ module.exports = {
       const gcMeta = await ev.groupMetadata(from)
       let dataConf = await getData(from.split("@")[0])
       if (typeof dataConf !== "object") dataConf = {}
-      let ppGroup = await ev.getProfilePicture(from).catch(() => 'https://tinyurl.com/yeon6okd'))
+      let ppGroup = await ev.getProfilePicture(from).catch(() => 'https://tinyurl.com/yeon6okd')
       let text = `\`\`\`\nSubject: ${gcMeta?.subject}\nOwner: ${gcMeta?.owner}\nID: ${gcMeta?.id}\nSize: ${gcMeta?.participants?.length}\n`
       text += `Created: ${new Date(gcMeta?.creation * 1000).toLocaleString()} \nWelcome: ${dataConf?.["join"]?.["active"] ? "ON" : "OFF"}\nLeft: ${dataConf?.["left"]?.["active"] ? "ON" : "OFF"}\n`
       text += `Desc:\n${gcMeta?.desc ? gcMeta?.desc?.toString() : 'Empty'}\`\`\``
