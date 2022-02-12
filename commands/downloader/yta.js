1325a1c6c987c0b3f5d2cb36f0189eb60afaf1df
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-12 14:15:02 +0700

    Update yta.js

--- commands/downloader/yta.js
+++ commands/downloader/yta.js
@@ -1,7 +1,6 @@
-const ytsr = require('ytsr')
 const lang = require('../other/text.json')
+const { fetchJson, textParse } = require('../../utils')
 const { validateURL } = require('../../utils/youtube-url-utils')
-const { fetchBuffer, fetchText, textParse } = require('../../utils')
 
 module.exports = {
 	name: 'yta',
@@ -15,30 +14,29 @@ module.exports = {
 			let { url, opt } = textParse(args.join(' '))
 			if (!validateURL(url)) return wa.reply(from, lang.eng.util.download.notYTURL, msg)
 			wa.reply(from, `IND:\n${lang.indo.util.download.progress}\n\nEN:\n${lang.eng.util.download.progress}`, msg)
-			let { items } = await ytsr(url)
+			let { result: res } = await fetchJson(`https://yt-downloader.akkun3704.repl.co/yt?url=${url}`)
 			let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highest&contenttype=audio/mp3`
-			// const res = await fetchBuffer(ytLink)
 			switch (opt) {
 				case '--doc':
-					if (items[0].duration.replace(/\D/g, '') > 10000) {
-						let caption = `*Title:* ${items[0].title}\n*Views:* ${items[0].views}\n*Duration:* ${items[0].duration}\n*Download:* ${ytLink}\n\n_Filesize too big_`
-						await wa.mediaURL(from, items[0].thumbnails[0].url, { quoted: msg, caption })
+					if (res.videoDetails.lengthSeconds > 3600) {
+						let caption = `*Title:* ${res.videoDetails.title}\n*Views:* ${res.videoDetails.viewCount}\n*Duration:* ${clockString(res.videoDetails.lengthSeconds)}\n*Download:* ${ytLink}\n\n_Filesize too big_`
+						await wa.mediaURL(from, `https://i.ytimg.com/vi/${res.videoDetails.videoId}/0.jpg`, { quoted: msg, caption })
 					} else {
-						wa.custom(from, { url: ytLink }, 'documentMessage', { mimetype: 'audio/mp4', filename: items[0].title + '.mp3', quoted: msg })
+						wa.custom(from, { url: ytLink }, 'documentMessage', { mimetype: 'audio/mp4', filename: res.videoDetails.title + '.mp3', quoted: msg })
 					}
 					break
 				case '--ptt':
-					if (items[0].duration.replace(/\D/g, '') > 10000) {
-						let caption = `*Title:* ${items[0].title}\n*Views:* ${items[0].views}\n*Duration:* ${items[0].duration}\n*Download:* ${ytLink}\n\n_Filesize too big_`
-						await wa.mediaURL(from, items[0].thumbnails[0].url, { quoted: msg, caption })
+					if (res.videoDetails.lengthSeconds > 3600) {
+						let caption = `*Title:* ${res.videoDetails.title}\n*Views:* ${res.videoDetails.viewCount}\n*Duration:* ${clockString(res.videoDetails.lengthSeconds)}\n*Download:* ${ytLink}\n\n_Filesize too big_`
+						await wa.mediaURL(from, `https://i.ytimg.com/vi/${res.videoDetails.videoId}/0.jpg`, { quoted: msg, caption })
 					} else {
 						wa.custom(from, { url: ytLink }, 'audioMessage', { mimetype: 'audio/mp4', ptt: true, quoted: msg })
 					}
 					break
 				default:
-					if (items[0].duration.replace(/\D/g, '') > 10000) {
-						let caption = `*Title:* ${items[0].title}\n*Views:* ${items[0].views}\n*Duration:* ${items[0].duration}\n*Download:* ${ytLink}\n\n_Filesize too big_`
-						await wa.mediaURL(from, items[0].thumbnails[0].url, { quoted: msg, caption })
+					if (res.videoDetails.lengthSeconds > 3600) {
+						let caption = `*Title:* ${res.videoDetails.title}\n*Views:* ${res.videoDetails.viewCount}\n*Duration:* ${clockString(res.videoDetails.lengthSeconds)}\n*Download:* ${ytLink}\n\n_Filesize too big_`
+						await wa.mediaURL(from, `https://i.ytimg.com/vi/${res.videoDetails.videoId}/0.jpg`, { quoted: msg, caption })
 					} else {
 						wa.custom(from, { url: ytLink }, 'audioMessage', { mimetype: 'audio/mp4', quoted: msg })
 					}
@@ -47,4 +45,11 @@ module.exports = {
 			wa.reply(msg.from, String(e), msg)
 		}
 	}
+}
+
+function clockString(ms) {
+	let h = isNaN(ms) ? '--' : Math.floor(ms % (3600 * 24) / 3600)
+	let m = isNaN(ms) ? '--' : Math.floor(ms % 3600 / 60)
+	let s = isNaN(ms) ? '--' : Math.floor(ms % 60)
+	return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
