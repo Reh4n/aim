288e5c33e8137286eb15b74b5b743361bc5423e3
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-13 23:37:25 +0700

    Update play.js

--- commands/downloader/play.js
+++ commands/downloader/play.js
@@ -15,7 +15,7 @@ module.exports = {
 		// const b = await compressImage(s[0].thumbnails[0].url)
 		let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${s[0].url}&filter=audioonly&quality=highest&contenttype=audio/mp3`
 		const struct = {
-			imageMessage: (await ev.prepareMessageMedia({ url: s[0].thumbnails.url }, 'imageMessage')).imageMessage,
+			imageMessage: (await ev.prepareMessageMedia({ url: s[0].thumbnails[0].url }, 'imageMessage')).imageMessage,
 			contentText: `📙 Title: ${s[0].title}\n📎 Url: ${s[0].url}\n🚀 Upload: ${s[0].uploadedAt}\n\nWant a video version? click button below, or you don\'t see it? type *!ytv youtube_url*\n\nAudio on progress....`,
 			footerText: 'Nyarlathotep-Bot ⬩ Made by XÆ15 - T',
 			headerType: 'IMAGE',
