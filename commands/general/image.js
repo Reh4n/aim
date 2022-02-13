c8d2cff9d64ab77ee25f89702e08caeb53df9d70
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-14 03:01:00 +0700

    Update image.js

--- commands/general/image.js
+++ commands/general/image.js
@@ -1,15 +1,18 @@
 const { googleImage } = require('@bochilteam/scraper')
+const { Whatsapp: ev } = require('../../core/connect')
 
 module.exports = {
   name: 'image',
   aliases: ['img', 'gimage', 'googleimage'],
   category: 'general',
   async execute(msg, wa, args) {
-    if (!args.join(' ')) return wa.reply(msg.from, 'Input Query', msg)
+    let query = args.join(' ')
+    if (!query) return wa.reply(msg.from, 'Input Query', msg)
+    query = query.endsWith('SMH') ? query.replace('SMH', '') : query
     await wa.reply(msg.from, 'Loading...', msg)
-    googleImage(args.join(' ')).then(res => {
+    googleImage(query).then(async (res) => {
       res = res[Math.floor(Math.random() * res.length)]
-      wa.mediaURL(msg.from, res, { quoted: msg, caption: `Hasil Pencarian: ${args.join(' ')}\nUrl: ${res}` })
+      wa.custom(msg.from, { imageMessage: (await ev.prepareMessageMedia({ url: res }, 'imageMessage')).imageMessage, contentText: `Result From: ${query}`, footerText: res, headerType: 'IMAGE', buttons: [{ buttonText: { displayText: 'Next' }, buttonId: `#image ${query} SMH`, type: 1 }] }, 'buttonsMessage', { quoted: msg })         
     }).catch(e => wa.reply(msg.from, String(e), msg))
   }
 }
