aa2ed88ec67cf47fdd68119ea02619692c6d2113
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-13 21:00:19 +0700

    Update fbdl.js

--- commands/downloader/fbdl.js
+++ commands/downloader/fbdl.js
@@ -1,22 +1,22 @@
-const { fbdl } = require("../../utils/downloader")
-const ev = require("../../core/connect").Whatsapp
-const lang = require("../other/text.json")
+const { facebookdlv2 } = require('@bochilteam/scraper')
+const lang = require('../other/text.json')
 
 const errMess = `ID:\n${lang.indo.util.download.fbFail}\n\nEN:\n${lang.eng.util.download.fbFail}`
 
 module.exports = {
-    name: "fb",
-    aliases: ["fbdl","facebook","fbvid"],
-    category: "Downloader",
-    desc: "Download Facebook video",
-    async execute(msg, wa, args) {
-        try {
-            let data = await fbdl(args[0])
-
-            if (data.length === 0) return wa.reply(msg.from, `ID:\n${lang.indo.util.download.fbPriv}\n\nEN:\n${lang.eng.util.download.fbPriv}`)
-            await ev.sendMessage(msg.from, { url: data[0] }, "videoMessage", { quoted: msg })
-        } catch(e) {
-            wa.reply(msg.from, errMess, msg)
-        }
+  name: 'fb',
+  aliases: ['fbdl', 'facebook', 'fbvid'],
+  category: 'Downloader',
+  desc: 'Download Facebook video',
+  async execute(msg, wa, args) {
+    try {
+      if (!args[0]) return wa.reply(msg.from, 'Input URL', msg)
+      let data = await facebookdlv2(args[0])
+      if (data.result.length === 0) return wa.reply(msg.from, `ID:\n${lang.indo.util.download.fbPriv}\n\nEN:\n${lang.eng.util.download.fbPriv}`)
+      await wa.custom(msg.from, { url: data.result[0].url }, 'videoMessage', { quoted: msg })
+    } catch(e) {
+      console.log(e)
+      wa.reply(msg.from, errMess, msg)
     }
+  }
 }
