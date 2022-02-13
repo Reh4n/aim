a0a3f6cfe4ac846647d30a9bb1cf18d3e335a553
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-14 03:07:22 +0700

    Update inspect.js

--- commands/misc/inspect.js
+++ commands/misc/inspect.js
@@ -13,11 +13,15 @@ module.exports = {
     let caption = `-- [Group Link Inspector] --\n\n*ID:* ${res.id}\n*Group Name:* ${res.subject}\n*Group Owner:* @${res.id.split('-')[0]}\n*Created at:* ${formatDate(res.creation * 1000)}\n${res.subjectOwner ? `*Group Name Changed by:* @${res.subjectOwner.split`@`[0]}`: ''}\n${res.descOwner ? `*Group Desc Changed by:* @${res.descOwner.split`@`[0]}` : ''}\n*Members Size:* ${res.size}\n${res.desc ? `*Desc:*\n${res.desc}` : ''}`.trim()
     let pp = await ev.getProfilePicture(res.id).catch(console.error)
     if (pp) return wa.mediaURL(msg.from, pp, { quoted: msg, caption })
-    wa.custom(msg.from, caption, 'extendedTextMessage', { quoted: msg, contextInfo: { mentionedJid: [...caption.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net') }})
+    wa.custom(msg.from, caption, 'extendedTextMessage', { quoted: msg, contextInfo: { mentionedJid: parseMentiot(caption) }})
   }
 }
 
 function formatDate(n, locale = 'id') {
   let d = new Date(n)
   return d.toLocaleDateString(locale, { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+}
+
+function parseMention(text) {
+  return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
 }
