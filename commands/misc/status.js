47c138e48b08bb55dcb4eb88116d9cec2f86c668
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-13 20:03:11 +0700

    Update status.js

--- commands/misc/status.js
+++ commands/misc/status.js
@@ -12,7 +12,7 @@ module.exports = {
         const group = chats.filter(v => !v.read_only && v.jid.endsWith('@g.us')).map(v => v.jid)
         let text = ''
         text += `HOST:\n- Arch: ${os.arch()}\n- CPU: ${os.cpus()[0].model}${os.cpus().length > 1 ? (' (' + os.cpus().length + 'x)') : ''}\n- Release: ${os.release()}\n- Version: ${os.version()}\n`
-        text += `- Memory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(os.totalmem / 1024 / 1024).toFixed(2)}MB\n`
+        text += `- Memory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(os.totalmem / 1024 / 1024).toFixed(2)} MB\n`
         text += `- Platform: ${os.platform()}\n- Hostname: ${os.hostname()}\n\nBOT:\n- Battery: ${ev['battery']['value'] == null ? 100 : ev['battery']['value']}% ${ev['battery']['charge'] ? '🔌Charging....' : '✅Stand by....'}\n`
         text += `- Chats:\n  - Personal: ${personal.length}\n  - Groups: ${group.length}`;
         wa.reply(msg.from, text, msg);
