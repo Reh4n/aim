8be1b5422aec6f5049a518f4f3c0486e10a47658
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-13 19:56:56 +0700

    Update join.js

--- commands/misc/join.js
+++ commands/misc/join.js
@@ -12,8 +12,8 @@ module.exports = {
         code = code[0].replace("chat.whatsapp.com/", "");
         // check invite code
         const check = await ev.query({ json: ["query","invite",code] });
-        if (check.status !== 200) return wa.reply(msg.from, "Invalid invite url.", msg);
-        //if (check.size < 80) return wa.reply(msg.from, "The minimum requirement for group members must be more than 80 people.", msg);
+        if (check.status !== 200) return wa.reply(msg.from, "Invalid invite url.\nMaybe bot was kicked from that's group.", msg);
+        if (check.size < 10) return wa.reply(msg.from, "The minimum requirement for group members must be more than 10 people.", msg);
         // Trying to join group with given invite code
         const check2 = await ev.acceptInvite(code);
         if (check2.status !== 200) return wa.reply(msg.from, "Looks like the group already full or became invalid when I'm trying to join :/", msg);
