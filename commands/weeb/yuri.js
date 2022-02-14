fd9737f18450323c1fd38cc17e31e12c7ea16df7
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-14 10:55:44 +0700

    Update yuri.js

--- commands/weeb/yuri.js
+++ commands/weeb/yuri.js
@@ -1,12 +1,13 @@
 const { fetchJson } = require('../../utils');
 
 module.exports = {
-    name: 'yuri',
-    category: 'weebs',
-    desc: 'Get random yuri image',
-    async execute(msg, wa) {
-        let list = ['yuri', 'eroyuri']
-        const { url } = await fetchJson(`https://nekos.life/api/v2/img/${list[~~(Math.random() * list.length)]}`);
-        wa.mediaURL(msg.from, url, { quoted: msg });
-    }
+  name: 'yuri',
+  category: 'weebs',
+  desc: 'Get random yuri image',
+  async execute(msg, wa) {
+    let list = ['yuri', 'eroyuri']
+    const { url } = await fetchJson(`https://nekos.life/api/v2/img/${list[~~(Math.random() * list.length)]}`)
+    if (msg.isGroup) return wa.custom(msg.from, { url }, 'imageMessage', { viewOnce: true })
+    else wa.mediaURL(msg.from, url, { quoted: msg })
+  }
 }
