82d545612df9ec0e12510e80ec233c0a0ccdeb10
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-14 10:18:58 +0700

    Update hentai.js

--- commands/weeb/hentai.js
+++ commands/weeb/hentai.js
@@ -6,7 +6,7 @@ module.exports = {
     desc: 'Random anime hentai.',
     async execute(msg, wa) {
         // if (msg.isGroup) return wa.reply(msg.from, "Only private chat", msg)
-        const r = await fetchJson('https://nekos.life/api/v2/img/hentai')
-        wa.mediaURL(msg.from, r.url, { quoted: msg })
+        const { url } = await fetchJson('https://nekos.life/api/v2/img/hentai')
+        wa.custom(msg.from, { url }, 'imageMessage', { quoted: msg, viewOnce: true })
     }
 }
