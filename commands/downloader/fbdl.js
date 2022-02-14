4fbd52578a2f978a0d22752d084a88851d80acc3
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-14 07:31:24 +0700

    Update fbdl.js

--- commands/downloader/fbdl.js
+++ commands/downloader/fbdl.js
@@ -5,7 +5,7 @@ const errMess = `ID:\n${lang.indo.util.download.fbFail}\n\nEN:\n${lang.eng.util.
 
 module.exports = {
   name: 'fb',
-  aliases: ['fbdl', 'facebook', 'fbvid'],
+  aliases: ['fbdl', 'facebook'],
   category: 'Downloader',
   desc: 'Download Facebook video',
   async execute(msg, wa, args) {
@@ -13,7 +13,7 @@ module.exports = {
       if (!args[0]) return wa.reply(msg.from, 'Input URL', msg)
       let data = await facebookdlv2(args[0])
       if (data.result.length === 0) return wa.reply(msg.from, `ID:\n${lang.indo.util.download.fbPriv}\n\nEN:\n${lang.eng.util.download.fbPriv}`)
-      await wa.custom(msg.from, { url: data.result[0].url }, 'videoMessage', { quoted: msg })
+      await wa.mediaURL(msg.from, data.result[0].url, { quoted: msg })
     } catch(e) {
       console.log(e)
       wa.reply(msg.from, errMess, msg)
