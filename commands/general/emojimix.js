fb6794866aebccc7631386b20c4d25fe4a6c1fcb
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-15 08:07:48 +0700

    Update emojimix.js

--- commands/general/emojimix.js
+++ commands/general/emojimix.js
@@ -9,7 +9,7 @@ module.exports = {
 		try {
 			let emo = args.join(' ').split('+')
 			if (!emo[0]) return wa.reply(msg.from, 'Emoji?', msg)
-			if (!emo[1]) return wa.reply(msg.from, 'Ex:\n!emojimix ðŸ˜±+ðŸ¥´', msg)
+			if (!emo[1]) return wa.reply(msg.from, 'Ex:\n!emojimix 😅+🙂', msg)
 			let res = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emo[0])}_${encodeURIComponent(emo[1])}`)
 			sticker((await fetchBuffer(res.results[0].url)), { isImage: true, withPackInfo: true, cmdType: '2', packInfo: { packname: 'EmojiMix', author: '' }})
 			.then((v) => wa.sticker(msg.from, v, { quoted: msg }))
