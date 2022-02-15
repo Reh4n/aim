b0e9739da77d77a1f6c13225d48d23fffc26af7c
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-02-15 13:38:27 +0700

    Update owner.js

--- commands/private/owner.js
+++ commands/private/owner.js
@@ -4,6 +4,6 @@ module.exports = {
 	name: 'owner',
 	category: 'misc',
 	execute(msg) {
-		ev.sendMessage(msg.from, { displayName: 'Owner', vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:LitRHap\nTEL;type=WORK;waid=628978365790:628978365790\nEND:VCARD`.trim() }, 'contactMessage', { quoted: msg })
+		ev.sendMessage(msg.from, { displayName: 'Owner', vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:LitRHap\nTEL;type=WORK;waid=818056745907:818056745907\nEND:VCARD`.trim() }, 'contactMessage', { quoted: msg })
 	}
 }
