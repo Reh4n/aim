6935a0904bd6d51f7240b4df7da45fdcfc92d478
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-02-16 17:15:46 +0700

    Update inspect.js

--- commands/misc/inspect.js
+++ commands/misc/inspect.js
@@ -9,7 +9,9 @@ module.exports = {
     if (!code) return wa.reply(msg.from, 'Link invalid', msg)
     let res = await ev.query({ json: ["query", "invite", code], expect200: true })
     if (!res) return wa.reply(msg.from, String(res), msg)
-    let caption = `*-- [Group Link Inspector] --*\n\n*ID:* ${res.id}\n*Group Name:* ${res.subject}\n*Group Owner:* @${res.id.split('-')[0]}\n*Created at:* ${formatDate(res.creation * 1000)}${res.subjectOwner ? `\n*Group Name Changed by:* @${res.subjectOwner.split`@`[0]}`: ''}${res.descOwner ? `\n*Group Desc Changed by:* @${res.descOwner.split`@`[0]}` : ''}\n*Members Size:* ${res.size}${res.desc ? `\n*Desc:*\n${res.desc}` : ''}`.trim()
+    let caption = `*-- [Group Link Inspector] --*\n\n*ID:* ${res.id}\n*Group Name:* ${res.subject}${res.id.includes('-') ? `\n*Group Owner:* @${res.id.split`-`[0]}` : `\n*Group Owner:* @${res.owner.split`@`[0]}`}\n`
+    caption += `*Created at:* ${formatDate(res.creation * 1000)}${res.subjectOwner ? `\n*Group Name Changed by:* @${res.subjectOwner.split`@`[0]}`: ''}\n`
+    caption += `${res.descOwner ? `\n*Group Desc Changed by:* @${res.descOwner.split`@`[0]}` : ''}\n*Members Size:* ${res.size}${res.desc ? `\n*Desc:*\n${res.desc}` : ''}`.trim()
     let pp = await ev.getProfilePicture(res.id).catch(console.error)
     if (pp) return wa.mediaURL(msg.from, pp, { quoted: msg, caption, contextInfo: { mentionedJid: parseMention(caption) }})
     wa.custom(msg.from, caption, 'extendedTextMessage', { quoted: msg, contextInfo: { mentionedJid: parseMention(caption) }})
