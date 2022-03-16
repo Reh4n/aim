51e21687bab59f970c30e99bf7e371c160076de0
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-03-16 13:59:59 +0700

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,33 +1,31 @@
 const djs = require('@discordjs/collection');
 const con = require('../../core/connect');
-const { calculatePing } = require('../../utils')
 const crypto = require('crypto')
-const os = require('os');
 const ev = con.Whatsapp;
 
 module.exports = {
   name: 'help',
   aliases: ['h', 'cmd', 'menu'],
-  category: 'General',
+  category: 'general',
   desc: 'show help message.',
   async execute(msg, wa, args) {
     if (args[0]) {
       const data = [];
       const name = args[0].toLowerCase();
-      const { commands, prefix } = djs;
+      const prefix = msg.body.slice(0, 1);
+      const { commands } = djs;
       const command = commands.get(name) || commands.find((cmd) => cmd.aliases && cmd.aliases.includes(name));
-      if (!command) return wa.reply(msg.from, `No Command of Alias Found || ${name}`, msg);
-      else data.push(`🎈 *Command:* ${command.name}`);
-      if (command.category) data.push(`⛩ *Category:* ${command.category}`);
-      if (command.aliases) data.push(`♦️ *Aliases:* ${command.aliases.join(', ')}`);
-      if (command.desc) data.push(`📒 *Description:* ${command.desc}`);
-      if (command.use) data.push(`💎 *Usage:* ${prefix}${command.name} ${command.use}\n\nNote: [] = optional, | = or, <> = must filled`);
+      if (!command) return wa.reply(msg.from, 'Perintah tidak tersedia.', msg);
+      else data.push(command.name);
+      if (command.aliases) data.push(`*Aliases:* ${command.aliases.join(', ')}`);
+      if (command.desc) data.push(`*Description:* ${command.desc}`);
+      if (command.use) data.push(`*Usage:* ${prefix}${command.name} ${command.use}\n\nNote: [] = optional, | = or, <> = must filled`);
 
       return wa.reply(msg.from, `${data.join('\n')}`, msg);
     } else {
-      const { sender, from, body } = msg;
+      const { sender, body } = msg;
+      const prefix = body.slice(0, 1);
       const { commands } = djs;
-      const prefix = body.slice(0, 1)
       const cmds = commands.keys()
       const pushname = msg.key.fromMe ? ev.user.name :
         ev.contacts[sender] !== undefined
@@ -37,44 +35,27 @@ module.exports = {
       for (let cmd of cmds) {
         let info = commands.get(cmd);
         if (!cmd) continue;
-        if (!info.category) continue;
+        if (!info.category || info.category === 'private') continue;
         if (Object.keys(categories).includes(info.category)) categories[info.category].push(info);
         else {
           categories[info.category] = [];
           categories[info.category].push(info);
         }
       }
-      let jam = new Date().toLocaleTimeString('id', { hour: '2-digit', timeZone: 'Asia/Jakarta' })
-      let salam = jam > 2 && jam < 4 ? 'Fajar' : jam < 11 ? 'Pagi' : jam < 16 ? 'Siang' : jam < 19 ? 'Sore' : 'Malam'
-      let str = `${ev.user.name ? ev.user.name : 'ArtoriaBot'}
-
-*🏠 Hostname*: ${os.hostname()}
-*🗓️ Tanggal*: ${new Date().toLocaleString('id', { dateStyle: 'full' })}
-*⏰ Runtime*: ${clockString(process.uptime())}
-*⚡ Speed*: ${calculatePing(msg.messageTimestamp, Date.now())}
-*🕵🏻‍♂️ Github*: LitRHap
-*👑 Instagram*: @a_p.th._.29
-
-👋🏻 Hello @${sender.split("@")[0]} Selamat ${salam}!\n\n`;
+      let str = '\t'.repeat(16) + "\`\`\`LitRHap\`\`\`\n\n"
+      +`Hello, @${sender.split("@")[0]}\n*Here My Command List*\n\n`;
       const keys = Object.keys(categories);
       for (const key of keys) {
-        str += `*${key}*\n${categories[key]
-          .map(command => `• ${prefix + command.name}`)
-          .join('\n')}\n\n`;
+        str += `*${key.toUpperCase()}*\n${categories[key]
+          .map((command) => `${prefix}${command.name}`)
+          .join(', ')}\n\n`;
       }
       str += `send ${prefix}help followed by a command name to get detail of command, e.g. ${prefix}help sticker`;
       let buttons = [
-        { buttonId: '#owner NPC', buttonText: { displayText: 'Owner' }, type: 1 },
-        { buttonId: '#stats NPC', buttonText: { displayText: 'Status' }, type: 1 }
+        { buttonId: '#owner SMH', buttonText: { displayText: 'OWNER' }, type: 1 },
+        { buttonId: '#stats SMH', buttonText: { displayText: 'STATUS' }, type: 1 }
       ]
       wa.sendButtons(msg.from, str, ' ', buttons, { quoted: msg, contextInfo: { mentionedJid: [sender] }})
     }
   },
 };
-
-function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms % (3600 * 24) / 3600)
-  let m = isNaN(ms) ? '--' : Math.floor(ms % 3600 / 60)
-  let s = isNaN(ms) ? '--' : Math.floor(ms % 60)
-  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
