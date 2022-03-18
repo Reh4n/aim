ef952a87f021ab3eb4ec93838c6fd03ac8652052
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-03-18 12:53:40 +0700

    Update uploader.js

--- utils/uploader.js
+++ utils/uploader.js
@@ -1,8 +1,10 @@
 const axios = require("axios").default;
 const cheerio = require("cheerio");
 const BodyForm = require("form-data");
+const Bluebird = require("bluebird");
+const path = require("path").join;
 const { fromBuffer } = require("file-type");
-const { fetchBuffer } = require("./index");
+const { fetchBuffer, formatSize, getRandom } = require("./index");
 const { createReadStream, unlinkSync, promises } = require("fs");
 
 const webp2mp4 = (path) => {
@@ -47,10 +49,10 @@ const webp2mp4 = (path) => {
  * @param {Buffer} fileData Your file
  * @param {"telegraph"|"uguu"|"anonfiles"} type File Hosting API
  */
-const uploaderAPI = (fileData, type) => new Promise(async (resolve, reject) => {
+const uploaderAPI = (fileData, type) => new Bluebird(async (resolve, reject) => {
     const postFile = async (fileData, type) => {
         const { ext, mime } = await fromBuffer(fileData);
-        const filePath = `utils/${mime.split("/")[0] + Math.floor(Date.now() / 1000)}.${ext}`;
+        const filePath = path("utils", mime.split("/")[0] + getRandom(`.${ext}`));
         const form = new BodyForm();
         await promises.writeFile(filePath, fileData);
         // Start Uploading
@@ -61,13 +63,13 @@ const uploaderAPI = (fileData, type) => new Promise(async (resolve, reject) => {
                 responseType: "json", headers: { ...form.getHeaders() }
             });
             if (data.error) reject(data.error);
-            return { host: "telegraph", data: { name: filePath.replace("utils/", ""), url: 'https://telegra.ph' + data[0].src, size: fileData.length } };
+            return { host: "telegraph", data: { name: filePath.replace("utils/", ""), url: 'https://telegra.ph' + data[0].src, size: formatSize(fileData.length) } };
         } else if (type === "uguu") {
             form.append("files[]", createReadStream(filePath));
             const { data } = await axios.post("https://uguu.se/upload.php", form, {
                 responseType: "json", headers: { ...form.getHeaders() }
             })
-            return { host: "uguu", data: { url: data.files[0].url, name: data.files[0].name, size: parseInt(data.files[0].size) } };
+            return { host: "uguu", data: { url: data.files[0].url, name: data.files[0].name, size: formatSize(parseInt(data.files[0].size)) } };
         } else if (type === "anonfiles") {
             form.append("file", createReadStream(filePath));
             const { data } = await axios.post("https://api.anonfiles.com/upload", form, {
@@ -75,11 +77,17 @@ const uploaderAPI = (fileData, type) => new Promise(async (resolve, reject) => {
             });
             if (!data.status) reject(data.error.message);
             return { host: "anonfiles", data: { url: data.data.file.url.short, name: data.data.file.metadata.name, size: data.data.file.metadata.size.readable } };
+        } else if (type === "ichika") {
+            form.append("file", createReadStream(filePath));
+            const { data } = await axios.post("https://ichikaa.xyz/upload", form, {
+                responseType: "json", headers: { ...form.getHeaders() }
+            });
+            return { host: "ichika", data: { url: data.result.url, name: data.result.originalname, size: data.result.size } };
         }
     }
     try {
         const result = await postFile(fileData, type);
-        unlinkSync("utils/" + result.data.name);
+        unlinkSync(path("utils", result.data.name));
         console.log("Success");
         resolve(result);
     } catch (e) { reject(e) }
@@ -92,7 +100,7 @@ const uploaderAPI = (fileData, type) => new Promise(async (resolve, reject) => {
  * @param {string} bottom 
  * @returns {Promise<Buffer>}
  */
-const memeText = (imageData, top, bottom) => new Promise(async (resolve, reject) => {
+const memeText = (imageData, top, bottom) => new Bluebird(async (resolve, reject) => {
     try {
         if (!imageData) reject('No imageData');
         const imageUrl = (await uploaderAPI(imageData, "uguu")).data.url;
@@ -100,8 +108,9 @@ const memeText = (imageData, top, bottom) => new Promise(async (resolve, reject)
         let bottomText = bottom.trim().replace(/\s/g, '_').replace(/\?/g, '~q').replace(/\%/g, '~p').replace(/\#/g, '~h').replace(/\//g, '~s');
 
         let result = `https://api.memegen.link/images/custom/${topText}/${bottomText}.png?background=${imageUrl}`;
-        const binResult = await fetchBuffer(result);
+        let binResult = await fetchBuffer(result);
         resolve(binResult);
+        binResult = null;
     } catch (e) {
         reject(e)
     }
