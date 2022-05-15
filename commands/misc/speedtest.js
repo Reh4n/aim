c4f12efc7fc97b0d8194ab65762f73adc0d034f4
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-05-15 20:02:49 +0700

    Create speedtest.js

---
+++ commands/misc/speedtest.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'speedtest',
+	category: 'general',
+	execute(msg, wa) {
+		require('child_process').exec('speed-test -j', (error, stdout) => {
+			if (error) return wa.reply(msg.from, String(error), msg)
+			// if (stderr) return wa.reply(msg.from, `Stderr: ${stderr}`, msg)
+			wa.reply(msg.from, `Output: ${stdout}`, msg)
+		})
+	}
+}
