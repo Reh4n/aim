0d2c0108c5511b7e41f9abcbd0dfa2024086fa93
Reh4n <89706310+Reh4n@users.noreply.github.com>
2022-05-20 20:56:03 +0700

    Update yuri.js

--- commands/weeb/yuri.js
+++ commands/weeb/yuri.js
@@ -1,12 +1,13 @@
-const { fetchJson } = require('../../utils');
+const Booru = require('booru')
 
 module.exports = {
   name: 'yuri',
   category: 'weebs',
   desc: 'Get random yuri image',
   async execute(msg, wa) {
-    // let list = ['yuri', 'eroyuri']
-    const { url } = await fetchJson(`https://hmtai.herokuapp.com/v2/nsfw/yuri`)
+    let list = ['sb', 'kn', 'kc'],
+      res = await Booru.search(list[~~(Math.random() * list.length)], ['yuri'], { random: true }),
+      url = res[0].fileUrl
     if (msg.isGroup) return wa.custom(msg.from, { url }, 'imageMessage', { viewOnce: true })
     else wa.mediaURL(msg.from, url, { quoted: msg })
   }
